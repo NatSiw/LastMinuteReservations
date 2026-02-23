bd32fce88e0cf64a541b3e6fbbf9e11a15daf4cc
Natalia <natalia.siwienki@gmail.com>
2026-02-23 03:18:34 +0100

    init

---
+++ src/main/java/com/example/lastminutereservation/LastMinuteReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.lastminutereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastMinuteReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastMinuteReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lastminutereservation/LastMinuteReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lastminutereservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastMinuteReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
